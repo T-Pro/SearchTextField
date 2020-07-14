2dec0ad8f2342f9ae18ec43d4131b03cf61c92f6
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2020-07-14 17:31:01 +0100

    Add SearchTextField generics

---
+++ Example/SearchTextField/StringSearchTextField.swift
@@ -0,0 +1,13 @@
+//
+//  StringSearchTextField.swift
+//  SearchTextField_Example
+//
+//  Created by Pedro Paulo de Amorim on 14/07/2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import SearchTextField
+
+@IBDesignable
+open class StringSearchTextField: SearchTextField<String> {}
